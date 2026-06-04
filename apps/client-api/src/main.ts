f35d16f29691ed9faf2097a530c160ba899542ee
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2026-06-04 20:37:35 +0400

    Add ports

--- apps/client-api/src/main.ts
+++ apps/client-api/src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from "@nestjs/core";
+import ports from "@repo/config-ports";
 
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 4000);
+  await app.listen(process.env.PORT ?? ports.clientApi);
 }
 
 void bootstrap();
---
+++ packages/config-ports/src/index.ts
@@ -0,0 +1,7 @@
+const ports = {
+  clientWeb: 3000,
+  clientApi: 4000,
+  serverApi: 4001,
+};
+
+export default ports;
