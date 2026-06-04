3ffced25d1d74921eda6e22d2afd52f4803a3667
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2026-06-04 13:57:44 +0400

    Fix

--- apps/server-api/src/app.controller.ts
+++ apps/server-api/src/app.controller.ts
@@ -1,13 +1,24 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
 
 import { AppService } from "./app.service";
+import {
+  ExecutionRequest,
+  ExecutionResponse,
+} from "typed-remote-procedure-call";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get("/health")
+  healthCheck(): string {
+    return "OK";
+  }
+
+  @Post("/rpc")
+  handleRequestFromCallerSide(
+    @Body() body: { request: ExecutionRequest },
+  ): Promise<ExecutionResponse> {
+    return this.appService.handleRequestFromCallerSide(body.request);
   }
 }
