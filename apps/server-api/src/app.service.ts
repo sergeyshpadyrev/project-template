f9e89ebd3b395a60a1ef6db6bc9314e51b1eda51
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2026-06-04 21:32:50 +0400

    Fix

--- apps/server-api/src/app.service.ts
+++ apps/server-api/src/app.service.ts
@@ -6,42 +6,34 @@ import {
 import { Injectable } from "@nestjs/common";
 import { ClientServerInterface, Project, Task } from "@repo/core-types";
 
+import { DatabaseService } from "./database.service";
+
 @Injectable()
 export class AppService {
+  constructor(private readonly database: DatabaseService) {}
+
   private executor = createExecutor<ClientServerInterface>({
-    createProject: (input: { name: string }): Promise<Project> => {
-      throw new Error("Method not implemented.");
-    },
+    createProject: (input: { name: string }): Promise<Project> =>
+      this.database.createProject(input),
     createTask: (input: {
       projectId: string;
       task: Omit<Task, "id">;
-    }): Promise<Task> => {
-      throw new Error("Method not implemented.");
-    },
-    deleteProject: (input: { projectId: string }): Promise<void> => {
-      throw new Error("Method not implemented.");
-    },
-    deleteTask: (input: { taskId: string }): Promise<void> => {
-      throw new Error("Method not implemented.");
-    },
-    getProjects: (): Promise<Project[]> => {
-      throw new Error("Method not implemented.");
-    },
-    getTasks: (input: { projectId: string }): Promise<Task[]> => {
-      throw new Error("Method not implemented.");
-    },
+    }): Promise<Task> => this.database.createTask(input),
+    deleteProject: (input: { projectId: string }): Promise<void> =>
+      this.database.deleteProject(input),
+    deleteTask: (input: { taskId: string }): Promise<void> =>
+      this.database.deleteTask(input),
+    getProjects: (): Promise<Project[]> => this.database.getProjects(),
+    getTasks: (input: { projectId: string }): Promise<Task[]> =>
+      this.database.getTasks(input),
     updateProject: (input: {
       projectId: string;
       name: string;
-    }): Promise<Project> => {
-      throw new Error("Method not implemented.");
-    },
+    }): Promise<Project> => this.database.updateProject(input),
     updateTask: (input: {
       taskId: string;
       task: Partial<Omit<Task, "id">>;
-    }): Promise<Task> => {
-      throw new Error("Method not implemented.");
-    },
+    }): Promise<Task> => this.database.updateTask(input),
   });
 
   async handleRequestFromCallerSide(
