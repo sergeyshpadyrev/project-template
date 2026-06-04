62af98247616fccdcf463935b370ea3a16e2cd7e
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2026-06-04 21:29:11 +0400

    Fix

--- packages/core-sdk/src/index.ts
+++ packages/core-sdk/src/index.ts
@@ -1,11 +1,17 @@
 import { ClientServerInterface } from "@repo/core-types";
 
-import { createRPC, ExecutionRequest } from "@repo/utils-rpc";
+import { createRPC, ExecutionRequest, ExecutionResponse } from "@repo/utils-rpc";
 
 const rpc = createRPC<ClientServerInterface>({
   send: async (request: ExecutionRequest) => {
-    console.log({ request });
-    return null as any;
+    const response = await fetch("http://localhost:4001/rpc", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ request }),
+    });
+    return (await response.json()) as ExecutionResponse;
   },
 });
 
