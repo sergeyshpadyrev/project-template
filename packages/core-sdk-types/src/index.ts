764e12620805567885519b30a1a9a0ddce81ff18
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2026-06-29 16:25:44 +0400

    Fix interfaces

--- packages/core-sdk-types/src/index.ts
+++ packages/core-sdk-types/src/index.ts
@@ -1,2 +1,16 @@
-export * from './entities';
-export * from './interfaces';
+// Models
+export * from './models/project';
+export * from './models/task';
+
+// Inputs
+export * from './inputs/createProject';
+export * from './inputs/createTask';
+export * from './inputs/deleteProject';
+export * from './inputs/deleteTask';
+export * from './inputs/getTasks';
+export * from './inputs/updateProject';
+export * from './inputs/updateTask';
+
+// Interfaces 
+export * from './interfaces/clientServer';
+
