86f2921f0112fc5f6b798cc695b28813c8281fe7
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2026-06-29 15:52:46 +0400

    Fix folders

---
+++ packages/core-sdk-types/src/types/models.ts
@@ -0,0 +1,14 @@
+export class Project {
+    id: string;
+    name: string;
+    tasks: Task[];
+}
+
+export type Task = {
+    description: string;
+    id: string;
+    name: string;
+    status: 'done' | 'in-progress' | 'todo';
+};
+
+
