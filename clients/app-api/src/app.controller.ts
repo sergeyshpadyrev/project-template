d5d2297aedb6d7264a195fd01f376d13f0b3b4ca
Sergei Shpadyrev <sergei.shpadyrev@beaconred.ae>
2026-08-20 14:40:45 +0400

    Add app api

--- clients/app-api/src/app.controller.ts
+++ clients/app-api/src/app.controller.ts
@@ -1,4 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import {
+  CreateProjectInput,
+  CreateTaskInput,
+  UpdateProjectInput,
+  UpdateTaskInput,
+} from '@repo/types-app';
 
 import { AppService } from './app.service';
 
@@ -6,9 +22,59 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
- 
+  @Post('/projects')
+  createProject(@Body() input: CreateProjectInput): ReturnType<AppService['createProject']> {
+    return this.appService.createProject(input);
+  }
+
+  @Post('/projects/:projectId/tasks')
+  createTask(
+    @Param('projectId') projectId: string,
+    @Body() input: Omit<CreateTaskInput, 'projectId'>
+  ): ReturnType<AppService['createTask']> {
+    return this.appService.createTask({ ...input, projectId });
+  }
+
+  @Delete('/projects/:projectId')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  deleteProject(@Param('projectId') projectId: string): ReturnType<AppService['deleteProject']> {
+    return this.appService.deleteProject({ projectId });
+  }
+
+  @Delete('/tasks/:taskId')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  deleteTask(@Param('taskId') taskId: string): ReturnType<AppService['deleteTask']> {
+    return this.appService.deleteTask({ taskId });
+  }
+
+  @Get('/projects')
+  getProjects(): ReturnType<AppService['getProjects']> {
+    return this.appService.getProjects();
+  }
+
+  @Get('/projects/:projectId/tasks')
+  getTasks(@Param('projectId') projectId: string): ReturnType<AppService['getTasks']> {
+    return this.appService.getTasks({ projectId });
+  }
+
   @Get('/health')
   healthCheck(): string {
     return 'OK';
+  }
+
+  @Patch('/projects/:projectId')
+  updateProject(
+    @Param('projectId') projectId: string,
+    @Body() input: Omit<UpdateProjectInput, 'projectId'>
+  ): ReturnType<AppService['updateProject']> {
+    return this.appService.updateProject({ ...input, projectId });
+  }
+
+  @Patch('/tasks/:taskId')
+  updateTask(
+    @Param('taskId') taskId: string,
+    @Body() input: Omit<UpdateTaskInput, 'taskId'>
+  ): ReturnType<AppService['updateTask']> {
+    return this.appService.updateTask({ ...input, taskId });
   }
 }
--- clients/app-api/src/app.service.ts
+++ clients/app-api/src/app.service.ts
@@ -1,4 +1,47 @@
 import { Injectable } from '@nestjs/common';
+import rpc from '@repo/sdk-app';
+import {
+  AppClientServerInterface,
+  CreateProjectInput,
+  CreateTaskInput,
+  DeleteProjectInput,
+  DeleteTaskInput,
+  GetTasksInput,
+  UpdateProjectInput,
+  UpdateTaskInput,
+} from '@repo/types-app';
 
 @Injectable()
-export class AppService {}
+export class AppService implements AppClientServerInterface {
+  createProject(input: CreateProjectInput): ReturnType<AppClientServerInterface['createProject']> {
+    return rpc.createProject(input);
+  }
+
+  createTask(input: CreateTaskInput): ReturnType<AppClientServerInterface['createTask']> {
+    return rpc.createTask(input);
+  }
+
+  deleteProject(input: DeleteProjectInput): ReturnType<AppClientServerInterface['deleteProject']> {
+    return rpc.deleteProject(input);
+  }
+
+  deleteTask(input: DeleteTaskInput): ReturnType<AppClientServerInterface['deleteTask']> {
+    return rpc.deleteTask(input);
+  }
+
+  getProjects(): ReturnType<AppClientServerInterface['getProjects']> {
+    return rpc.getProjects();
+  }
+
+  getTasks(input: GetTasksInput): ReturnType<AppClientServerInterface['getTasks']> {
+    return rpc.getTasks(input);
+  }
+
+  updateProject(input: UpdateProjectInput): ReturnType<AppClientServerInterface['updateProject']> {
+    return rpc.updateProject(input);
+  }
+
+  updateTask(input: UpdateTaskInput): ReturnType<AppClientServerInterface['updateTask']> {
+    return rpc.updateTask(input);
+  }
+}
