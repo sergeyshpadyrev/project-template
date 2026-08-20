2bdef4f698c3f02d61c7abc3b13171be3ce91613
Sergei Shpadyrev <sergei.shpadyrev@beaconred.ae>
2026-08-20 14:23:58 +0400

    Fix

--- clients/dashboard-web/vite.config.ts
+++ clients/dashboard-web/vite.config.ts
@@ -5,6 +5,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: ports.dashboardWeb,
+    port: ports.dashboardClientWeb,
   },
 });
--- packages/config-ports/index.ts
+++ packages/config-ports/index.ts
@@ -1,8 +1,10 @@
 const ports = {
-    appApi: 3000,
-    appWeb: 3001,
-    dashboardApi: 4000,
-    dashboardWeb: 4001,
+    appCore: 3000,
+    appClientApi: 3001,
+    appClientWeb: 3002,
+    dashboardCore: 4000,
+    dashboardClientApi: 4001,
+    dashboardClientWeb: 4002,
 };
 
 export default ports;
